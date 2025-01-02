4e202b05d497c994eb2dee6d9baa525e2b66f5cc
Arman Mujawar <armanmujawar999@gmail.com>
2025-01-02 15:59:29 +0530

    fist commit

---
+++ src/components/Cart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Cart = () => {
+  return (
+    <>
+     <div className='py-4'>
+         <div>hello</div>
+         <div>hello</div>
+         <div>hello</div>
+     </div>
+      
+    </>
+  )
+}
+
+export default Cart
